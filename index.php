af0cdb5e02f43ca6e92870f32ad3fd6b35a4678d
ctraganos <ctraganos@yahoo.com>
2010-11-25 23:28:41 -0600

    Edits for updating the ID and link back to twitter. In addition, adding the photo and tweet layout.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
+<?php include("includes/functions.php");?>
 <!DOCTYPE html>
- <html lang="">
+ <html lang="en">
  <head>
  	<title>#CIRCA</title>
 
@@ -44,7 +45,9 @@
 
 		 	</div><!-- exit #highlights-->
 		 	<div id="content">
-				<h1>HTML Ipsum Presents</h1>
+				<h1>#dwts</h1>
+				<?php tweet_fetch("ctraganos",10); ?>
+				<?php tweet_fetch("tristanwalker",10); ?>
 					       
 				<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 				
