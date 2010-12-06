88c6f9b20db030659706f7e2e8f1da193e017f86
ctraganos <ctraganos@yahoo.com>
2010-12-05 22:09:45 -0500

    Edits for having the application accept an array of usernames and return the sorted stream along with caching each json feed locally.

--- includes/functions.php
+++ includes/functions.php
@@ -10,55 +10,29 @@ function tweetCompress($username){
 		or die("Can't open file\n");
 
 	$tweets = json_decode(@file_get_contents(dirname(__FILE__)."/cache/{$username}-twitter.json"));
-	
 	$user_cache = json_encode($tweets);	
-	
 	$result = fwrite ($fileHandle, $user_cache);
-	
-	if ($result)
-	{
-	     echo "Data written successfully.<br>";
-	} else {
-	     echo "Data write failed.<br>";
-	}
-	
+
+	echo ($result) ? "Data written successfully.<br>" : "Data write failed.<br>";
+
 	fclose($fileHandle);
 
 }
 
-function pullCache($username, $username2){
-// $tweets = json_decode(@file_get_contents(dirname(__FILE__)."/cache/circa.json"));
-	$tweets[1] = json_decode(@file_get_contents(dirname(__FILE__)."/cache/{$username}-twitter.json"));
-	$tweets[2] = json_decode(@file_get_contents(dirname(__FILE__)."/cache/{$username2}-twitter.json"));
-	
-	$combined_tweets = array_merge($tweets[1]->results, $tweets[2]->results);
-	echo "<strong>First tweet:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	</strong> ";
-	echo strtotime($combined_tweets[0]->created_at)." aka: {$combined_tweets[0]->created_at}";
-	echo "<br><strong>Second tweet: </strong>";
-	echo strtotime($combined_tweets[9]->created_at)." aka: {$combined_tweets[8]->created_at}";
-	echo "<br>The ".cmp_date($combined_tweets[0]->created_at,$combined_tweets[1]->created_at)." tweet is newer<br>";
+function pullCache($users){
+	for($a = 0; $a < count($users); $a++){
+		tweetFetch($users[$a]);
+		$tweets[$a] = json_decode(@file_get_contents(dirname(__FILE__)."/cache/{$users[$a]}-twitter.json"))->results;
+		//$combined_tweets = array_push($combined_tweets,$tweets[$a]);
+	}
+	$combined_tweets = array_merge($tweets[0],$tweets[1],$tweets[2],$tweets[3],$tweets[4],$tweets[5]);
+
+	//$combined_tweets = array_merge($tweets[0]->results, $tweets[1]->results,$tweets[2]->results,$tweets[3]->results);
 	usort($combined_tweets, 'cmp_date');
-	echo "<h3>Sorted</h3>";
-	outputFeed($combined_tweets);
-	echo "<pre>";
-	 echo print_r($combined_tweets);
-	echo "</pre>";
-		
+	outputFeed($combined_tweets);		
 }
-
-
-function cmp_date($a, $b){
-    // if (strtotime($a->created_at) == strtotime($b->created_at)) {
-    if (strtotime($a->created_at) == strtotime($b->created_at)) {
-        return 0;
-    }
-    return (strtotime($a->created_at) > strtotime($b->created_at)) ? -1 : 1;
-}
-
-
-
-function tweetFetch($username, $num){
-	$feed = "http://search.twitter.com/search.json?q=from:" . $username . "&amp;rpp=" . $num;
+function tweetFetch($username){
+	$feed = "http://search.twitter.com/search.json?q=from:" . $username . "&amp;rpp=100";
 	 
 	$newfile = dirname(__FILE__)."/cache/{$username}-twitternew.json";
 	$file = dirname(__FILE__)."/cache/{$username}-twitter.json";
@@ -68,16 +42,66 @@ function tweetFetch($username, $num){
 	$oldcontent = @file_get_contents($file);
 	$newcontent = @file_get_contents($newfile);
 	 
+	if($oldcontent != $newcontent) copy($newfile, $file);
+}
+
+
+function cmp_date($a, $b){
+    if (strtotime($a->created_at) == strtotime($b->created_at)) {
+        return 0;
+    }
+    return (strtotime($a->created_at) > strtotime($b->created_at)) ? -1 : 1;
+}
+
+/*  
+Function twitterStream()
+Pulls in tweets for specific topic requested.
+*/
+function twitterStream($keyword){
+	$feed = "http://search.twitter.com/search.json?geocode=42.380054%2C-71.132952%2C50.0mi&lang=en&q=+{$keyword}+since%3A2010-11-26+until%3A2010-11-26+near%3A02138+within%3A50mi&rpp=100";
+	$newfile = dirname(__FILE__)."/cache/terms/{$keyword}-twitternew.json";
+	$file = dirname(__FILE__)."/cache/terms/{$keyword}-twitter.json";
+	
+	copy($feed, $newfile);
+	$oldcontent = @file_get_contents($file);
+	$newcontent = @file_get_contents($newfile);
+
 	if($oldcontent != $newcontent) {
 	copy($newfile, $file);
 	}
-	$tweets = @file_get_contents($file);
-	 
-	$tweets = json_decode($tweets);
+	$tweets = json_decode(@file_get_contents($file));
+	 echo count($tweets->results);
 	outputFeed($tweets->results);
+	}
 
+function outputFeed($combined_tweets){
+	for($x =0; $x < count($combined_tweets); $x++){	
+		$thumb = $combined_tweets[$x]->profile_image_url;
+		$created = ago(strtotime($combined_tweets[$x]->created_at));
+		$user = $combined_tweets[$x]->from_user;
+		$tweet_id = $combined_tweets[$x]->id_str;
+		$tweet = makeRich($combined_tweets[$x]->text);
+		?>
+		<div class="chunk">
+			<a href="http://twitter.com/<?php echo $user;?>" class="avatar">
+				<img src="<?php echo $thumb;?>" width="48" height="48" title="<?php echo $user;?>" /></a>
+			<span class="tweet">
+				<span class="user"><a href="http://twitter.com/<?php echo $user;?>"><?php echo $user;?></a></span>
+				<br /><?php echo $tweet; ?>
+			</span>
+			<span class="date">
+				<a href="http://twitter.com/<?php echo $user;?>/status/<?php echo $tweet_id;?>/"><?php echo $created; ?></a>
+			</span>
+		</div><!-- exit div.chunk -->
+
+	<?php // continue structure
+	}
 
 }
+/* 
+Asthetic Formatting for the Streams
+*/
+
 /*  
 Function ago()
 Based on script from snipt.net that compares
@@ -123,50 +147,5 @@ function makeRich($text)
 	return $text;
 }
 
-/*  
-Function twitterStream()
-Pulls in tweets for specific topic requested.
-*/
-function twitterStream($keyword){
-	$feed = "http://search.twitter.com/search.json?geocode=42.380054%2C-71.132952%2C50.0mi&lang=en&q=+{$keyword}+since%3A2010-11-26+until%3A2010-11-26+near%3A02138+within%3A50mi&rpp=100";
-	$newfile = dirname(__FILE__)."/cache/terms/{$keyword}-twitternew.json";
-	$file = dirname(__FILE__)."/cache/terms/{$keyword}-twitter.json";
-	
-	copy($feed, $newfile);
-	$oldcontent = @file_get_contents($file);
-	$newcontent = @file_get_contents($newfile);
 
-	if($oldcontent != $newcontent) {
-	copy($newfile, $file);
-	}
-	$tweets = json_decode(@file_get_contents($file));
-	 echo count($tweets->results);
-	outputFeed($tweets->results);
-	}
-
-function outputFeed($combined_tweets){
-	for($x =0; $x < count($combined_tweets); $x++){	
-		$thumb = $combined_tweets[$x]->profile_image_url;
-		$created = ago(strtotime($combined_tweets[$x]->created_at));
-		$created2 = strtotime($combined_tweets[$x]->created_at);
-		$user = $combined_tweets[$x]->from_user;
-		$tweet_id = $combined_tweets[$x]->id_str;
-		$tweet = makeRich($combined_tweets[$x]->text);
-		?>
-		<div class="chunk">
-			<a href="http://twitter.com/<?php echo $user;?>" class="avatar">
-				<img src="<?php echo $thumb;?>" width="48" height="48" title="<?php echo $user;?>" /></a>
-			<span class="tweet">
-				<span class="user"><a href="http://twitter.com/<?php echo $user;?>"><?php echo $user;?></a></span>
-				<br /><?php echo $tweet; ?>
-			</span>
-			<span class="date">
-				<a href="http://twitter.com/<?php echo $user;?>/status/<?php echo $tweet_id;?>/"><?php echo $created; ?>(<?php echo $created2; ?>)</a>
-			</span>
-		</div><!-- exit div.chunk -->
-
-	<?php // continue structure
-	}
-
-}
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include("includes/functions.php");?>
+<?php define(TERM, "harvard");?>
 <!DOCTYPE html>
  <html lang="en">
  <head>
  	<title>#CIRCA</title>
 
  	<meta charset="utf-8">
- 	<meta name="description" content="" />
+ 	<meta name="description" content="A website for social media" />
  	<meta name="keywords" content="" />
  	<meta name="robots" content="" />
  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -45,35 +46,27 @@
 
 		 	</div><!-- exit #highlights-->
 		 	<div id="content">
+				
 				<h1>Wall O' Tweets</h1>
-					       
-				<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
-				
-				<h2>Header Level 2</h2>
-					       
-				<ol>
-				   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-				   <li>Aliquam tincidunt mauris eu risus.</li>
-				</ol>
-				
-				<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-				
-				<h3>Header Level 3</h3>
 				<div class="scroll">
-					<?php twitter_stream("harvard"); ?>
+				<?php
+				echo TERM; // Testing a constant that will be the term to filter results for
+				
+				/*	This is where I construct an array of twitter users for the app to fetch and sort 
+				*	Eventually this will be constructed from the set of approved users.
+				* 	In the meantime, an array of usernames are passed into the function for processing
+				*/
+				$users = array("techcrunch","mashable","msnbc_tech","guardiantech","forbestech","nytimestech");
+				?>
+				<?php pullCache($users); ?>
 				</div>
+				<div class="clearfix"></div>       
 				<ul>
 				   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
 				   <li>Aliquam tincidunt mauris eu risus.</li>
 				</ul>
-				
-				<pre><code>
-				#header h1 a { 
-					display: block; 
-					width: 300px; 
-					height: 80px; 
-				}
-				</code></pre>
+
+
 		 	</div><!-- exit #content -->
 		 	<div id="footer">
 		 	
